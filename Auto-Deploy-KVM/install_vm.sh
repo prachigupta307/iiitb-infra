1a678ce50a2f1af9cf836a3c5398184f11a6761b
Prasanna Kumar <PrasannaKumar.Nimbagallu@iiitb.org>
2020-04-12 15:22:41 +0530

    Install_vm.sh is updated

--- Auto-Deploy-KVM/install_vm.sh
+++ Auto-Deploy-KVM/install_vm.sh
@@ -3,14 +3,21 @@
 hostIp=$(hostname -I | awk '{print $1}')
 systemctl start httpd
 
+VM_NAME="testvm1"
+RAM=1024
+CPUS=1
+IMAGE_NAME="testvm1-os.qcow2"
+SIZE="5"
+
+
 #keep the OS image in /osmedia
 virt-install  	--network bridge:virbr0 \
-		--name testvm1 \
+		--name $VM_NAME \
 		--os-variant=centos7.0 \
-		--ram=1024 \
-		--vcpus=1  \
- 		--disk path=/var/lib/libvirt/images/testvm1-os.qcow2,format=qcow2,bus=virtio,size=5 \
+		--ram=$RAM \
+		--vcpus=$CPUS  \
+ 		--disk path=/var/lib/libvirt/images/$IMAGE_NAME,format=qcow2,bus=virtio,size=$SIZE \
   		--graphics none \
 	      	--location=/osmedia/CentOS-7-x86_64-DVD-1810.iso \
   		--extra-args="console=tty0 console=ttyS0,115200"  -x ks=http://$hostIp/install/ks.cfg \
-		--check all=off \
+		--check all=off 
