1c14afb454a86d2ce25e664dc42ea438e0a8e57d
as1212742 <36498783+as1212742@users.noreply.github.com>
2020-05-03 13:50:39 +0530

    ansible kvm(vm) creation and fetching of kvm ip
    initial commit from there ansible script initiates

---
+++ ansible_playbooks/fetch_ip_of_vm.sh
@@ -0,0 +1,12 @@
+#VIRTUAL_MACHINE_NAME=$(sudo virsh list --name)
+sudo rm -rf hosts1
+sudo touch hosts1
+echo [all] >>  hosts1
+
+for VIRTUAL_MACHINE_NAME in $(sudo virsh list --name); do
+
+result=$(arp -an | grep $(virsh dumpxml $VIRTUAL_MACHINE_NAME | grep '<mac' | grep -o '\([0-9a-f][0-9a-f]:\)\+[0-9a-f][0-9a-f]') | grep -o '\([0-9]\{1,3\}\.\)\+[0-9]\{1,3\}')
+echo $result ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=admin >> hosts1 
+
+done
+
