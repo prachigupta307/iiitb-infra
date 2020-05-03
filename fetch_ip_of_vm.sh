20293bba102f63658d692cc7bfe821d48ba589bc
as1212742 <36498783+as1212742@users.noreply.github.com>
2020-05-03 14:14:48 +0530

    Update fetch_ip_of_vm.sh

--- fetch_ip_of_vm.sh
+++ fetch_ip_of_vm.sh
@@ -6,7 +6,7 @@ echo [all] >>  hosts1
 for VIRTUAL_MACHINE_NAME in $(sudo virsh list --name); do
 
 result=$(arp -an | grep $(virsh dumpxml $VIRTUAL_MACHINE_NAME | grep '<mac' | grep -o '\([0-9a-f][0-9a-f]:\)\+[0-9a-f][0-9a-f]') | grep -o '\([0-9]\{1,3\}\.\)\+[0-9]\{1,3\}')
-echo $result ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=admin >> hosts1 
+echo $result ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=admin >> hosts
 
 done
 
